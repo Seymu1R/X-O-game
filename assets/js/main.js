6b1b2c32600c096ffc3eb621457566fb1c529cb1
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-08-10 20:19:41 +0400

    adding functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,40 @@
-let cellClic=document.querySelectorAll(".cell");
-cellClic.forEach(item=>{
-    item.addEventListener("click",()=>{
-        console.log("salam");
+let cellClick = document.querySelectorAll(".cell");
+let dataSpans = document.querySelectorAll('[data-cell]');
+const viningCombination = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+]
+let content;
+cellClick.forEach(item => {
+    item.addEventListener("click", (e) => {
+        if (e.target.innerText == "") {
+            if (content == "X") {
+                e.target.firstElementChild.innerText = "O"
+                content = "O";
+                if (checkWin(content)) {
+                    console.log("vin");
+                }
+            }
+            else {
+                e.target.firstElementChild.innerText = "X";
+                content = "X";
+                if (checkWin(content)) {
+                    console.log("vin");
+                }
+            };
+        }
     })
 })
+let checkWin = (content) => {
+
+    return viningCombination.some(
+        combination=>{
+            return combination.every(index=>{
+                return dataSpans[index].innerHTML=content;
+            })
+        }
+    )
+};
+
+
