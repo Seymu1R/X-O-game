f32aeb3cd8f977004ac05bfa44b6e8e56528a13a
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-08-10 23:01:31 +0400

    correct variant

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,36 +5,52 @@ const viningCombination = [
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6]
 ]
+const options = ["", "", "", "", "", "", "", "", ""]
+
 let content;
 cellClick.forEach(item => {
     item.addEventListener("click", (e) => {
         if (e.target.innerText == "") {
             if (content == "X") {
-                e.target.firstElementChild.innerText = "O"
+                let contentSpan = e.target.firstElementChild;
+                contentSpan.innerText = "O"
                 content = "O";
-                if (checkWin(content)) {
-                    console.log("vin");
-                }
+                let cellIndex = contentSpan.getAttribute("cellIndex");
+                uptadeCell(cellIndex, content);
+                checkWin();
+
             }
             else {
-                e.target.firstElementChild.innerText = "X";
+                let contentSpan = e.target.firstElementChild;
+                contentSpan.innerText = "X";
                 content = "X";
-                if (checkWin(content)) {
-                    console.log("vin");
-                }
+                let cellIndex = contentSpan.getAttribute("cellIndex");
+                uptadeCell(cellIndex, content);
+                checkWin();
+
             };
         }
     })
 })
-let checkWin = (content) => {
-
-    return viningCombination.some(
-        combination=>{
-            return combination.every(index=>{
-                return dataSpans[index].innerHTML=content;
-            })
-        }
-    )
+let checkWin = () => {
+    let roundWon = false;
+    for (const item of viningCombination) {
+        const cellA = options[item[0]];
+        const cellB = options[item[1]];
+        const cellC = options[item[2]];
+        if (cellA == "" || cellA == "" || cellC == "") {
+            continue;
+        };
+        if (cellA === cellB && cellC === cellB) {
+            roundWon=true;
+        }    }
+    if (roundWon==true) {
+    console.log("win");
+        
+    }
 };
+let uptadeCell = (cellIndex, content) => {
+    options[cellIndex] = content;
+}
 
 
